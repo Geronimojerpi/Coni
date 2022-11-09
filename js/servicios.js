e12d306520b747a2692e87e444aa6f2cfc11f99b
Geronimo Jerpi <gero_jerpi_50@hotmail.com>
2022-11-09 02:46:06 -0300

    Second Commit

---
+++ js/servicios.js
@@ -0,0 +1,13 @@
+function irServicios(){
+
+    window.addEventListener('scroll', ()=>{
+        const scroll = document.documentElement.scrollTop;
+        console.log(scroll);
+        let botonServicios = document.getElementById('botonServicios');
+        if{
+            
+        }
+
+    })
+}
+irServicios()
